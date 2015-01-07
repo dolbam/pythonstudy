c9e90b65442ddccca28ca336ac0e76a8ead5b125
dolbam <dolbam@H3002958A.CJI.COM>
2015-01-07 17:49:04 +0900

    study

--- python_study_2015/simpleP.py
+++ python_study_2015/simpleP.py
@@ -1,1 +1,12 @@
 print 'Hello, world!'
+name = raw_input('What is your name?\n')
+print 'Hi, %s.' % name
+
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print "iteration {iteration} is {name}".format(iteration=i, name=name)
+    
+parents, babies = (1, 1)
+while babies < 100:
+    print 'This generation has {0} babies'.format(babies)
+    parents, babies = (babies, parents + babies)    
